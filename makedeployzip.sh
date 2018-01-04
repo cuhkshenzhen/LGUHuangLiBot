ea9f595cbd59b8532e4f97bb97a619a731ba6573
Cnly <minecnly@gmail.com>
2018-01-04 19:47:29 +0800

    Exclude boto3 things from deploy.zip

--- makedeployzip.sh
+++ makedeployzip.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 rm -f deploy.zip
-zip -9 -r deploy.zip . --exclude 'venv/*' '.*/*' '.*' '__pycache__/*'
+zip -r deploy.zip . --exclude 'venv/*' '.*/*' '.*' '__pycache__/*'
 cd venv/lib/python*/site-packages || exit 1
-zip -9 -r ../../../../deploy.zip * --exclude 'setuptools-*/*' 'setuptools/*' 'pip-*/*' 'pip/*' 'pkg_resources-*/*' 'pkg_resources/*' 'wheel-*/*' 'wheel/*' '__pycache__/*' '*-info/*' 'easy_install.py'
+zip -r ../../../../deploy.zip * --exclude 'setuptools-*/*' 'setuptools/*' 'pip-*/*' 'pip/*' 'pkg_resources-*/*' 'pkg_resources/*' 'wheel-*/*' 'wheel/*' '__pycache__/*' '*-info/*' 'easy_install.py' 'boto3-*/*' 'boto3/*' 'botocore-*/*' 'botocore/*' 'docutils-*/*' 'docutils/*' 's3transfer/*' 's3transfer-*/*' 'dateutil-*/*' 'dateutil/*' 'docutils/*' 'docutils-*/*' 'jmespath-*/*' 'jmespath/*'
 cd ../../../../
